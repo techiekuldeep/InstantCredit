5ddb171aad3136a97aa36d7d921d8c0ea9a7d73a
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-14 00:26:01 +1200

    Added utility classes for AppSettings.

---
+++ InstantCredit/Utility/AppSettingsClasses/InstantForecastSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstantCredit.Utility.AppSettingsClasses
+{
+    public class InstantForecastSettings
+    {
+        public string ForecastTrackerEnabled { get; set; }
+    }
+}
