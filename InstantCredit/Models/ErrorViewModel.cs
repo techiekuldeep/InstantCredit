7e2549c05e7e6f816f3717d9323bf5939a3326e8
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-13 23:16:25 +1200

    Add project files.

---
+++ InstantCredit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InstantCredit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
