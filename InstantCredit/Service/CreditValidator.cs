df8402d8c1fa12f7fa5c19fb311c48cb8c66a28d
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-14 16:39:12 +1200

    Added CreditValidator class and ICreditValidator interface.

---
+++ InstantCredit/Service/CreditValidator.cs
@@ -0,0 +1,35 @@
+﻿using InstantCredit.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstantCredit.Service
+{
+    public class CreditValidator : ICreditValidator
+    {
+        private readonly IEnumerable<IValidationChecker> _validations;
+        public CreditValidator(IEnumerable<IValidationChecker> validations)
+        {
+            _validations = validations;
+        }
+        public async Task<(bool, IEnumerable<string>)> PassAllValidations(CreditApplication model)
+        {
+            bool validationsPassed = true;
+            List<string> errorMessages = new List<string>();
+
+            foreach (var validation in _validations)
+            {
+                if (!validation.ValidatorLogic(model))
+                {
+                    //Error
+                    errorMessages.Add(validation.ErrorMessage);
+                    validationsPassed = false;
+                }
+            }
+
+            return (validationsPassed, errorMessages);
+        }
+    }
+}
---
+++ InstantCredit/Service/ICreditValidator.cs
@@ -0,0 +1,14 @@
+﻿using InstantCredit.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InstantCredit.Service
+{
+    public interface ICreditValidator
+    {
+        Task<(bool, IEnumerable<string>)> PassAllValidations(CreditApplication model);
+    }
+}
